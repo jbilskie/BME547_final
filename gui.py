cb9e81543bdd144f62845688cc09c316c2cd0e4e
Xinlin Chen <xinlin.chen@duke.edu>
2019-04-18 06:11:45 -0400

    Added GUI widgets

--- gui.py
+++ gui.py
@@ -1,2 +1,77 @@
 from tkinter import *
 from tkinter import ttk
+import re
+
+
+def image_window():
+    # Main window
+    root = Tk()
+    root.title("Process Your Image")
+
+    top_label = ttk.Label(root, text='Process Your Image On Our Server!')
+    top_label.grid(column=0, row=0, columnspan=2, sticky=N)
+
+    # Enter username
+    user_label = ttk.Label(root, text="Username:")
+    user_label.grid(column=0, row=1, sticky=E)
+    user = StringVar()
+    user_entry = ttk.Entry(root, textvariable=user)
+    user_entry.grid(column=1, row=1)
+
+    # Enter image paths
+    img_label = ttk.Label(root, text="Image paths:")
+    img_label.grid(column=0, row=2, sticky=E)
+    img_path = StringVar()
+    img_path_entry = ttk.Entry(root, textvariable=img_path)
+    img_path_entry.grid(column=1, row=2)
+
+    # Check processing steps
+    steps_label = ttk.Label(root, text="Processing steps")
+    steps_label.grid(column=0, row=4, sticky=E)
+
+    hist_eq = StringVar()
+    contr_stretch = StringVar()
+    log_comp = StringVar()
+    rev_vid = StringVar()
+
+    hist_check = ttk.Checkbutton(root, text='Histogram Equalization',
+                                 variable=hist_eq,
+                                 onvalue=True, offvalue=False)
+    hist_check.grid(column=1, row=4, sticky=W)
+    contr_check = ttk.Checkbutton(root, text='Contrast Stretching',
+                                  variable=contr_stretch,
+                                  onvalue=True, offvalue=False)
+    contr_check.grid(column=1, row=5, sticky=W)
+    log_check = ttk.Checkbutton(root, text='Log Compression',
+                                variable=log_comp,
+                                onvalue=True, offvalue=False)
+    log_check.grid(column=1, row=6, sticky=W)
+    rev_check = ttk.Checkbutton(root, text='Reverse video',
+                                variable=rev_vid,
+                                onvalue=True, offvalue=False)
+    rev_check.grid(column=1, row=7, sticky=W)
+
+    display_img = StringVar()
+    display_check = ttk.Checkbutton(root, text='Display images',
+                                    variable=display_img,
+                                    onvalue=True, offvalue=False)
+
+    # Select download image type
+    img_type = StringVar()
+    type_label = ttk.Label(root, text="Download image type:")
+    type_label.grid(column=0, row=3, sticky=E)
+    # Add readonly flag
+    type_dropdown = ttk.Combobox(root, textvariable=img_type)
+    type_dropdown['values'] = ('JPEG', 'PNG', 'TIFF')
+    type_dropdown.grid(column=1, row=3)
+
+    ok_btn = ttk.Button(root, text='OK', width=5)
+    ok_btn.grid(column=1, row=8)
+
+    # Show GUI window
+    root.mainloop()
+    return
+
+
+if __name__ == "__main__":
+    image_window()
