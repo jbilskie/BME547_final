526b911ea8755b7d0c0e9ea70e38f1f7c6fa1bc4
Xinlin Chen <xinlin.chen@duke.edu>
2019-04-20 01:32:17 +0800

    test for process_new_user

---
+++ test_server.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request
+import numpy as np
+import pytest
+from pymodm import connect, MongoModel, fields
+from server import *
+
+
+@pytest.mark.parametrize("input, exp", [({"username": ""},
+                                         {"code": 400}),
+                                        ({"username": "rando"},
+                                         {"code": 200}),
+                                        ({"username": "1239"},
+                                         {"code": 200})])
+def test_process_new_user(input, exp):
+    """Tests process_new_user
+
+    Tests whether new user request is correctly processed. From
+    the GUI functions, the username is captured as a string, so
+    integer fields aren't tested. The original function also
+    assumes the "username" key exists, because the GUI sends a
+    string for username even if nothing was inputted.
+
+    Args:
+        input ():
+
+    Returns:
+        exp ():
+    """
+    if exp["code"] == 200:
+        exp["msg"] = "Request was successful"
+    elif exp["code"] == 400:
+        exp["msg"] = "Field username cannot be empty."
+    status = process_new_user(input)
+    assert status == exp
